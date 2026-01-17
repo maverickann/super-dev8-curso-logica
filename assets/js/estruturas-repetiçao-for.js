1a0e08a0e8d93e3f68f5f90cdfff7dd94c29b8bc
maverickann <viniciuseslopes@gmail.com>
2026-01-17 18:52:48 -0300

    fim

---
+++ assets/js/estruturas-repetiçao-for.js
@@ -0,0 +1,26 @@
+function usoFor() {
+    //condiçao
+    while () {
+
+    }
+
+}
+function relogio() {
+    for (let hora = 0; hora < 24; hora + 1) {
+        for (let minuto = 0; minuto < 60; minuto + 1) {
+            for (let segundo = 0; segundo < 60; segundo + 1) {
+                
+
+            }
+
+
+        }
+
+    }
+}
+function exemploDeContagemRegressiva() {
+    for (let indice = 10; indice >=0; indice-1) {
+        alert(indice);
+        
+    }
+}
